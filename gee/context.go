ffd499a7df69fee698493f1877f555d93e35b40f
ljy <1257389967@qq.com>
2021-05-18 16:16:32 +0800

    update

--- gee/context.go
+++ gee/context.go
@@ -1,3 +1,76 @@
 package gee
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+//map[string] interface是值 key是string value是interface{}，即是value可以是任意值
+//这里把map[string] interface取了个别名H
 type H map[string]interface{}
+
+type Context struct {
+	Writer http.ResponseWriter
+	Req    *http.Request
+	//对path和method提供直接访问
+	Path   string
+	Method string
+
+	StatusCode int
+}
+
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method,
+	}
+}
+
+//返回Post里存放的信息
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	// WriteHeader函数就是用来写code的
+	// The provided code must be a valid HTTP 1xx-5xx status code.
+	c.Writer.WriteHeader(code)
+}
+
+func (c *Context) SetHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+func (c *Context) Html(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
